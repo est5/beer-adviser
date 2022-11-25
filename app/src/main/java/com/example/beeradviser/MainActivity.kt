b5bb17ad6c4ac8e4ffee2b1f5b0b9aa889bab314
est5 <ghostrekon5500@ya.ru>
2022-11-25 18:51:27 +0300

    Beer list

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.beeradviser
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem
+            val beerList = getBeers(color.toString())
+            val beers = beerList.reduce { str, item -> str + '\n' + item }
+            val brands = findViewById<TextView>(R.id.brands)
+            brands.text = beers
+        }
+    }
+
+    private fun getBeers(color: String) = when (color) {
+        "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+        "Amber" -> listOf("Jack Amber", "Red Moose")
+        "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+        else -> listOf("Baltika 7", "Ohota krepkaya")
     }
 }
